c75387a419973a49dbe58ae1470227d08e13ab55
asoftcouch <45211335+asoftcouch@users.noreply.github.com>
2020-12-07 15:38:36 -0600

    added components and CSS

---
+++ app-digiqs/src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Nav = () => {
+    return(
+        <nav>
+            <h1></h1>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </nav>
+    )
+}
