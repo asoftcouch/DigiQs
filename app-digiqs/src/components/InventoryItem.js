9b1b108463b15305e024907d06091d581fa71a16
asoftcouch <45211335+asoftcouch@users.noreply.github.com>
2020-12-10 15:56:11 -0600

    added axios

--- app-digiqs/src/components/InventoryItem.js
+++ app-digiqs/src/components/InventoryItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-const InventoryItem = () => {
+const InventoryItem = (props) => {
     return(
         <div className="Inventory-item">
 
--- app-digiqs/src/components/InventoryList.js
+++ app-digiqs/src/components/InventoryList.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import inventoryItem from '../components/InventoryItem';
+import { Link } from 'react-router-dom'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 
 const InventoryList = () => {
+    const name = "example";
+    const quantity = 2; 
+
     return(
         <div>
             {/* //Aqui van todos mis objetos  */}
-            <inventoryItem /> 
+            <inventoryItem name={name} quantity={quantity} /> 
         </div>
     )
 }
