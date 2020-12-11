2ecbc44ff844811aca3dc845c0e57658a5588eea
asoftcouch <45211335+asoftcouch@users.noreply.github.com>
2020-12-11 15:55:43 -0600

    commit

--- app-digiqs/src/components/createInventory.js
+++ app-digiqs/src/components/createInventory.js
@@ -1,12 +1,32 @@
-import react from 'react';
+import React, { useEffect, useState } from 'react';
 import {Link} from 'react-router-dom'; 
 import Style from '../styles/createInventory.css'
+import axios from 'axios';
 
 const CreateInventory = () => {
 
+    const [ newList , setNewList] = useState({name: '', quantity: '', price: '', category: ''});
+    
+    const submitListHandler = async (e) => {
+        e.preventDefault(); 
+             setNewList( {   
+                        name: document.getElementById('name').value,
+                        quantity: document.getElementById('quantity').value,
+                        price: document.getElementById('price').value,
+                        category: document.getElementById('category').value 
+                        },
+                        sendData());   
+    }
+
+    const sendData = () => {
+        axios.post('http://localhost:5000/inventario/add',{name: newList.name, quantity: newList.quantity})
+        .then((response) => console.log(response))
+        .catch((error)=>console.log(error))
+    }
+    
     return(
         <div>
-            <form>
+            <form onSubmit={submitListHandler}>
                 <h3>Agrega un nuevo articulo: </h3>
                 <div className="createInventory-form">
                     <div className="form-input">
@@ -21,8 +41,14 @@ const CreateInventory = () => {
                         <label htmlFor="product-price">Precio unitario: </label>
                         <input type="text" name="price" id="price"/>
                     </div>
+                    <div className="form-input">
+                        <label htmlFor="product-price">Categoria: </label>
+                        <input type="text" name="category" id="category"/>
+                    </div>
                 </div>
-                <input className="submit-form" type="submit"/>
+                <div className="form-input-submit">
+                    <input className="submit-form" type="submit" value="Agregar nuevo item"/>
+                </div>
             </form>
         </div>
     )
